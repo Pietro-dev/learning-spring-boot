67a591354f6b5510d710397ff329331f4422097b
Pietro-dev <pietro.pietromartins@gmail.com>
2025-04-04 21:52:07 -0300

    endpoint post criado

--- src/main/java/com/produtos/produtos/api/controller/ProdutoController.java
+++ src/main/java/com/produtos/produtos/api/controller/ProdutoController.java
@@ -1,14 +1,30 @@
 package com.produtos.produtos.api.controller;
 
 import com.produtos.produtos.api.model.Produto;
+import com.produtos.produtos.api.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto){
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        produtoRepository.save(produto);
         return produto;
     }
+
+
 }
